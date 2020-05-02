ee54868b15f4bd7fcbb5a67e6749d1942ec0397e
Pyrex-FWI <yemistikris@hotmail.fr>
2020-05-02 18:56:33 +0200

    Test patch monorepo-builder release patch

--- Component/id3/src/Exception/Id3Exception.php
+++ Component/id3/src/Exception/Id3Exception.php
@@ -2,6 +2,11 @@
 
 namespace Sapar\Component\Id3\Exception;
 
+/**
+ * Class Id3Exception
+ * @author Pyrex-FWI <pyrex-fwi@gmail.com>
+ * 02/05/2020
+ */
 class Id3Exception extends \Exception
 {
 }
